861008431c3b135aa7d176249e8ee1c7a4a7fa68
Anuj Kumar <108259936+Buildwithus@users.noreply.github.com>
2023-08-25 00:44:33 +0530

    Update project.js

--- src/component/project/project.js
+++ src/component/project/project.js
@@ -66,7 +66,7 @@ function Project() {
                 <div className='p-p1'>
                     <div className='p-p1left' >
                    
-                        <h1 className='h1'><a href='https://anujblogg.netlify.app/'>1.Blog Website </a></h1>
+                        <h1 className='h1'><a href='https://anujblogg.netlify.app/'>4.Blog Website </a></h1>
                         <h4 className='hfour'>Description:</h4>
                         <p className='s-p1' data-aos="zoom-in">In this Project technology used React JS, Node JS, MongoDB, Express JS and CSS. User can create their Blog and there is search functionality
                          in this application. </p>
@@ -81,7 +81,7 @@ function Project() {
 
                 <div className='p-p1'>
                     <div className='p-p1left' >
-                        <h1 className='h1'><a href='https://mycrudapplication.netlify.app/'>2.CRUD Application </a></h1>
+                        <h1 className='h1'><a href='https://mycrudapplication.netlify.app/'>5.CRUD Application </a></h1>
                         <h4 className='hfour'>Description:</h4>
                         <p className='s-p1' data-aos="zoom-in">This is complete MERN Project. View button by the help of this button users
 can see their detail in other page.</p>
@@ -97,7 +97,7 @@ can see their detail in other page.</p>
 
                 <div className='p-p1'>
                     <div className='p-p1left' >
-                        <h1 className='h1'><a href='https://anujkumar-weather.netlify.app/'>3.Weather Application</a></h1>
+                        <h1 className='h1'><a href='https://anujkumar-weather.netlify.app/'>6.Weather Application</a></h1>
                         <h4 className='hfour'>Description:</h4>
                         <p className='s-p1' data-aos="zoom-in">In this Project the technology used Only React JS. Users can serach the live temperature of any city. In search bar make sure that the first letter must be capital.</p>
                     </div>
@@ -111,7 +111,7 @@ can see their detail in other page.</p>
 
                 <div className='p-p1'>
                     <div className='p-p1left' >
-                        <h1 className='h1'> <a href='https://anujkumarmusic.netlify.app/'>4.Music Website </a></h1>
+                        <h1 className='h1'> <a href='https://anujkumarmusic.netlify.app/'>7.Music Website </a></h1>
                         <h4 className='hfour'>Description:</h4>
                         <p className='s-p1' data-aos="zoom-in">In this Project the Technology used Only React Js and CSS. Songs and their images not dynamic.</p>
                     </div>
